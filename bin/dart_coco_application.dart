0a17beeb3e656b7a29bb16f4443c0d2296be93e9
Antony Cristobal <antony.cristobalz@pragma.com.co>
2024-05-14 10:54:26 -0500

    Initial Commit

---
+++ bin/dart_coco_application.dart
@@ -0,0 +1,5 @@
+import 'package:dart_coco_application/dart_coco_application.dart' as dart_coco_application;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_coco_application.calculate()}!');
+}
---
+++ test/dart_coco_application_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_coco_application/dart_coco_application.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
